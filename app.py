e9a83a03b9da69f83508fc0535026d9b248a419f
Mideva-Alma <aileenmideva@email.com>
2026-08-19 09:05:56 +0300

    patch and delete routes

--- app.py
+++ app.py
@@ -64,7 +64,39 @@ def add_inventory_item():
 
     return new_item, 201
 
+@app.route("/inventory/<int:item_id>", methods=["PATCH"])
+def update_inventory_item(item_id):
+    data = request.get_json()
 
+    for item in inventory:
+        if item["id"] == item_id:
+            if "product_name" in data:
+                item["product_name"] = data["product_name"]
+
+            if "brand" in data:
+                item["brand"] = data["brand"]
+
+            if "price" in data:
+                item["price"] = data["price"]
+
+            if "stock" in data:
+                item["stock"] = data["stock"]
+
+            if "barcode" in data:
+                item["barcode"] = data["barcode"]
+
+            return item
+
+    return {"error": "Inventory item not found"}, 404
+
+@app.route("/inventory/<int:item_id>", methods=["DELETE"])
+def delete_inventory_item(item_id):
+    for item in inventory:
+        if item["id"] == item_id:
+            inventory.remove(item)
+            return {"message": "Inventory item deleted successfully"}
+
+    return {"error": "Inventory item not found"}, 404
 
 
 if __name__ == "__main__":
